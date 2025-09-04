3a6a50e8474b0c5fdfe0d78baf148d7d07d32fbe
Samarth <officialdevelopersamarth@gmail.com>
2025-09-04 12:45:17 +0530

    lecture 8 and 9 code

---
+++ Lecture-9/arrays/app.js
@@ -0,0 +1,13 @@
+
+// let arr = [1,2,3,true,false,null,undefined,"sam"];
+
+// console.log(arr);
+// console.log(arr[3]);
+// console.log(arr[6]);
+
+// -----------------------------------------
+let arr = [1,2,3,true, [1000,[20000,30000,40000],3000],undefined,"sam"];
+
+
+console.log( arr );
+
---
+++ Lecture-9/variable/app.js
@@ -0,0 +1,27 @@
+// let naam = "Samarth Vohra"; console.log(naam)
+
+// let num = 9560780335; console.log(num)
+
+
+// let isMale = true; console.log(isMale)
+
+// let arrayyy = [10,20,"sam" , "maverick" , true , null]
+// let arrayyy2 = [10,20,30];
+
+// console.log(arrayyy);
+// console.log(arrayyy[3]);
+// console.log(arrayyy2[2]);
+
+
+let jaanwar = {
+    type : "Dog" ,
+    color : "brown",
+    legs : 4,
+    isNonVegetarian : true
+}
+
+console.log(jaanwar);
+// console.log(jaanwar[color]);
+console.log(jaanwar['color']);
+console.log(jaanwar.color);
+
